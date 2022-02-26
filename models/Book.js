919ee073e60c85764fcbd2525d3925ab8442eee3
vaishnavipatil2408 <99817801+vaishnavipatil2408@users.noreply.github.com>
2022-02-26 20:37:51 +0530

    Create Book.js

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+      required: [true, 'Book category is required'],
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
